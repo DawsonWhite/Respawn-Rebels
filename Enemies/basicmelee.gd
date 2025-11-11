c0489f5f4cf4830e0584b5aa11d104e687e3045f
DawsonWhite <jimmywhite877@gmail.com>
2025-11-11 17:31:22 -0500

    added weapon stuff

--- Enemies/basicmelee.gd
+++ Enemies/basicmelee.gd
@@ -43,8 +43,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		ani_player.play("attack")
 
-
-
-
 func _on_timer_timeout() -> void:
 	ani_player.play("run")
---
+++ prefabs/Arrow/arrow.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+class_name Arrow
+
+var target : CharacterBody2D
+var target_coords : Vector2
+const flight_speed := 200
+
+func _ready() -> void:
+	target_coords = target.position
+	
+func _physics_process(delta: float) -> void:
+	var direction = position.direction_to(target_coords)
+	velocity = direction + flight_speed
+	var angle = direction.angle()
+	rotaion = angle
